4fe76503a708490328693e8f8baba939f080333a
gengorounotiti-wq <gengorounotiti@gmail.com>
2026-01-29 12:10:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # Supabase 接続
 # ===============================
 SUPABASE_URL = "https://bqyvsdjmvkrhqrqzkwje.supabase.co"
-SUPABASE_KEY = "sb_secret_ufuuc9uiRE2b0P5pFpusaw_F3enTUk1"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqaGR1aXNpZnlyeGNsaWdjcGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NTk4ODAsImV4cCI6MjA4NTEzNTg4MH0.cCvrAG0_xLWlOi_ydZ5Vf6MQB7ddrIssJ7-nXGA1n7k"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # ===============================
